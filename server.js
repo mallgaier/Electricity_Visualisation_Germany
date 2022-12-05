cd7d14cdb891b86192da6bba117c32304f81839a
Michael Allgaier <michael.allgaier@tum.de>
2022-12-05 19:57:15 +0100

    Remove heroku-hosting for release

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/da-vis-electricity-production-germany'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/da-vis-electricity-production-germany/index.html'));});
-app.listen(process.env.PORT || 8080);
